1663aefd05b987dee76f7d0018bf104b04eda65f
lukasz-kam <lukasz.kaminski02@gmail.com>
2024-11-13 12:06:28 +0100

    Add example nodejs app, add terraform and ansible configuration

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "ssh_key_filename" {
+  description = "Path to ssh key file."
+  default = "gh_ansible.pem"
+}
+
+variable "gh_key_name" {
+  description = "Name of the ssh key"
+  default = "gh_key"
+}
